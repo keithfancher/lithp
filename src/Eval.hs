9ba31c080880aa7268166e77e902b5d048b1f41b
ktf <not.discostoo@gmail.com>
2022-12-05 20:40:09 -0800

    Add conditional logic!

--- src/Eval.hs
+++ src/Eval.hs
@@ -9,6 +9,11 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", predicate, conseq, alt]) = do
+  result <- eval predicate
+  case result of
+    Bool False -> eval alt
+    _otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
