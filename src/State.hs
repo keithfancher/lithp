dcc05641890093fb25a839e22e843553408b3194
ktf <not.ktf@proton.me>
2023-04-25 22:18:45 -0700

    Initial code for setting/getting state, aka: variables!

---
+++ src/State.hs
@@ -0,0 +1,71 @@
+module State
+  ( Env,
+    nullEnv,
+    defineVar,
+    getVar,
+    setVar,
+  )
+where
+
+import Control.Monad.Except (ExceptT, liftIO, runExceptT, throwError)
+import Data.IORef
+import Data.Maybe (isJust)
+import Error (LispError (..), ThrowsError, extractValue, trapError)
+import Val (LispVal)
+
+type Env = IORef [(String, IORef LispVal)]
+
+nullEnv :: IO Env
+nullEnv = newIORef []
+
+type IOThrowsError = ExceptT LispError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+runIOThrows :: IOThrowsError String -> IO String
+runIOThrows action = runExceptT (trapError action) >>= return . extractValue
+
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return . isJust . lookup var
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do
+  env <- liftIO $ readIORef envRef
+  maybe
+    (throwError $ UnboundVar "Getting an unbound variable" var)
+    (liftIO . readIORef)
+    (lookup var env)
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do
+  env <- liftIO $ readIORef envRef
+  maybe
+    (throwError $ UnboundVar "Setting an unbound variable" var)
+    (liftIO . flip writeIORef value)
+    (lookup var env)
+  return value
+
+-- Handle the special behavior of `define`, which sets a variable if already
+-- bound or creates a new one if not.
+-- See: https://conservatory.scheme.org/schemers/Documents/Standards/R5RS/HTML/r5rs-Z-H-8.html#%_sec_5.2
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do
+  alreadyDefined <- liftIO $ isBound envRef var
+  if alreadyDefined
+    then setVar envRef var value >> return value
+    else liftIO $ do
+      valueRef <- newIORef value
+      env <- readIORef envRef
+      writeIORef envRef ((var, valueRef) : env)
+      return value
+
+-- Bind many variables at once.
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+  where
+    extendEnv bindings' env = fmap (++ env) (mapM addBinding bindings')
+    addBinding (var, value) = do
+      ref <- newIORef value
+      return (var, ref)
