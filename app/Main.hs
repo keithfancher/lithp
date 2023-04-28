8bae8f7db41c2be666f4be34eaeccdfe4d881e58
ktf <not.ktf@proton.me>
2023-04-27 20:48:14 -0700

    Rejigger to avoid use of partial function `head`

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import System.Environment (getArgs)
 main :: IO ()
 main = do
   args <- getArgs
-  case length args of
-    0 -> runRepl
-    1 -> runOne $ head args -- TODO: partial!
+  case args of
+    [] -> runRepl
+    [expr] -> runOne expr
     _ -> putStrLn "Program takes only 0 or 1 argument"
