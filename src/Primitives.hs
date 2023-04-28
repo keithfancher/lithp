0c6c0c187a8fc3d320981113843589f05ef78da0
ktf <not.ktf@proton.me>
2023-04-27 22:18:12 -0700

    Refactor: pull out a helper function, explicitly call out an undefined case

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -96,17 +96,23 @@ eqv [Number arg1, Number arg2] = return $ Bool $ arg1 == arg2
 eqv [String arg1, String arg2] = return $ Bool $ arg1 == arg2
 eqv [Atom arg1, Atom arg2] = return $ Bool $ arg1 == arg2
 eqv [DottedList xs x, DottedList ys y] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
-eqv [List arg1, List arg2] =
-  return $
-    Bool $
-      (length arg1 == length arg2)
-        && all eqvPair (zip arg1 arg2)
+eqv [List arg1, List arg2] = return $ Bool $ listsEqualStrict arg1 arg2
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
+-- Check for equality of two lists of `LipsVal`s: they're the same length, and
+-- each corresponding item is also (strictly) equivalent.
+listsEqualStrict :: [LispVal] -> [LispVal] -> Bool
+listsEqualStrict list1 list2 =
+  (length list1 == length list2)
+    && all eqvPair (zip list1 list2)
   where
     eqvPair (x1, x2) = case eqv [x1, x2] of
       Left _ -> False
       Right (Bool val) -> val
-eqv [_, _] = return $ Bool False
-eqv badArgList = throwError $ NumArgs 2 badArgList
+      -- TODO: This final case can never happen as the code is currently written.
+      -- Need to find a good way for the types to actually reflect that!
+      _ -> undefined
 
 data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
 
@@ -127,5 +133,6 @@ equal [arg1, arg2] = do
         (unpackEquals arg1 arg2)
         [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
   eqvEquals <- eqv [arg1, arg2]
+  -- TODO: This incomplete match is also due to lying types, same as above. Fix!
   return $ Bool (primitiveEquals || let (Bool x) = eqvEquals in x)
 equal badArgList = throwError $ NumArgs 2 badArgList
